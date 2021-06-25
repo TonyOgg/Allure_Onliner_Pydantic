c75b37cad0ecdbdadea83a541aa07676cd49a753
Anton M <70973886+TonyOgg@users.noreply.github.com>
2021-06-25 10:43:20 +0300

    Update Pet_swagger.py

--- Pet_swagger.py
+++ Pet_swagger.py
@@ -22,76 +22,51 @@ petty = """{
   "status": "available"
 }"""
 
-class Categoies_fields(BaseModel):
+class Category(BaseModel):
     id: int
     name: str
 
-
-class Tags_fields(BaseModel):
+class Tags(BaseModel):
     id: int
     name: str
 
 
 class Pets(BaseModel):
     id: int
-    category: dict[Categoies_fields]
+    category: Category
     name: str
     photoUrls: list[str]
-    tags: dict[Tags_fields]
+    tags: list[Tags]
     status: str
 
 
 pet = Pets.parse_raw(petty)
 print(pet)
 
-pet_1 = Pets(222333,
-    {
-    16,
-    "Cat"
-  },
-  "Oudrey",
-  [
-    'https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg'
-  ],
-  [
-    {
-      15,
-      "Catty"
-    }
-  ],
-  "available"
-)
+pet_1 = Pets(222333, {16, "Cat"}, "Oudrey", 
+             'https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg', 
+             {15, "Catty"}, "available")
 
-pet_1_new = Pets(222333,
-    {
-    16,
-    "Kittens"
-  },
-  "doggie",
-  [
-    'https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg'
-  ],
-  [
-    {
-      15,
-      "Catty"
-    }
-  ],
-  "available"
-)
+pet_1_new = Pets(222333, {15, "Kitten"}, "Oudrey",
+             'https://cs9.pikabu.ru/post_img/2017/05/12/8/1494592816133830021.jpg',
+             {15, "Catty"}, "available")
+
 
 def test_creating_pet():
     response = requests.post(address, json=pet_1.json())
     assert response.status_code == 200
 
+
 def test_is_pet_in():
-    response = requests.post(address + f'/%{}'.format(pet_1.id))
+    response = requests.post(address + f'/{0}'.format(pet_1.id))
     assert response.status_code == 200
+
 
 def test_updating_pet():
     response = requests.put(address, json=pet_1_new.json())
     assert response.status_code == 200
 
+
 def test_delete_pet():
-    response = requests.post(address + f'/%{}'.format(pet_1.id))
+    response = requests.post(address + f'/{0}'.format(pet_1_new.id))
     assert response.status_code == 200
